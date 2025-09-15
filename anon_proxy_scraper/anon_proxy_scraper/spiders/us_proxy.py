0f3d805cbd164517069fd49d7247b146eccb79be
Gabriel Silva Carvalho <gccomputacao@gmail.com>
2025-09-15 15:40:51 -0300

    first: versão do curso

---
+++ anon_proxy_scraper/anon_proxy_scraper/spiders/us_proxy.py
@@ -0,0 +1,25 @@
+import scrapy
+
+class UsProxySpider(scrapy.Spider):
+    name = "us_proxy"
+
+    def start_requests(self):
+        urls = ["https://www.us-proxy.org/"]
+        
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        table = response.xpath("//table[@class='table table-striped table-bordered']//tr")
+
+        for line in table:
+            yield {
+                "ip_address": line.xpath("./td[1]/text()").get(),
+                "port": line.xpath("./td[2]/text()").get(),
+                "code": line.xpath("./td[3]/text()").get(),
+                "country": line.xpath("./td[4]/text()").get(),
+                "anonymity": line.xpath("./td[5]/text()").get(),
+                "google": line.xpath("./td[6]/text()").get(),
+                "https": line.xpath("./td[7]/text()").get(),
+                "last_checked": line.xpath("./td[8]/text()").get()
+            }
